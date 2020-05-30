b58a09c6cebf2316772b8d09dbb8c3ef6443bc79
Hejianxiong <jianxiong2017@gmail.com>
2020-05-30 16:12:06 +0800

    add

---
+++ gin/uploadfile/single.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	// 为 multipart forms 设置较低的内存限制 (默认是 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.POST("/upload", func(c *gin.Context) {
+		// 单文件
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		// 上传文件至指定目录
+		c.SaveUploadedFile(file, "uploadfile/a.tar")
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+	router.Run(":8080")
+}
